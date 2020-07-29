0effebe92b58683e70eb1789838d8ebf8c467123
ch04937 <97hernandez.c@gmail.com>
2020-07-29 18:44:36 -0500

    feat: updates users profile

--- src/routes/router/profile.js
+++ src/routes/router/profile.js
@@ -16,16 +16,33 @@ router.get("/", verifyUser, async (req, res) => {
 });
 
 // change profile db
-router.put("/:profile_uuid", verifyUser, async (req, res) => {
-	const body = req.body;
-	const profileId = req.params.profile_uuid;
+router.put("/change", verifyUser, async (req, res) => {
+	const changes = req.query;
+	const profileId = req.user.userId;
 	try {
-		const post = await Profile.update(profileId, body);
-		res.status(202).json({ post });
+		const post = await Profile.update(profileId, changes);
+		res.status(202).json(post);
 	} catch (e) {
-		console.log("e", e);
 		res.status(404).json({ message: `could not find ${e}`, e: e });
 	}
+});
+
+router.put("/ncc", verifyUser, async (req, res) => {
+	const changes = req.body;
+	const profileId = req.user.userId;
+	try {
+		const post = await Profile.update(profileId, changes);
+		res.status(202).json(post);
+	} catch (e) {
+		res.status(404).json({ message: `could not find ${e}`, e: e });
+	}
+});
+
+router.put("/img", verifyUser, async (req, res) => {
+	const img = req.body;
+	const profileId = req.user.userId;
+
+	console.log("img", profileId, img);
 });
 
 module.exports = router;
